86dae524ab51001698b90b3a39c0921c5106173b
Tom Flídr <tomflidr@gmail.com>
2021-01-22 10:52:29 +0100

    new version

--- src/MvcCore/Ext/Tools/Image.php
+++ src/MvcCore/Ext/Tools/Image.php
@@ -20,7 +20,7 @@ abstract class Image implements \MvcCore\Ext\Tools\Images\IImage {
 	 * Comparison by PHP function version_compare();
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.0.0';
+	const VERSION = '5.0.1';
 
 	/**
 	 * @var int
